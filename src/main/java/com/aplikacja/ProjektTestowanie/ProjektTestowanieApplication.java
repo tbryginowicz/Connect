a121275bdfb4f49e99ea13f41a50511e4aacb834
lukasz <lukasz.kocurek@edu.uekat.pl>
2024-03-19 10:59:32 +0100

    lol

---
+++ src/main/java/com/aplikacja/ProjektTestowanie/ProjektTestowanieApplication.java
@@ -0,0 +1,13 @@
+package com.aplikacja.ProjektTestowanie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektTestowanieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektTestowanieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aplikacja/ProjektTestowanie/ProjektTestowanieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplikacja.ProjektTestowanie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektTestowanieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
